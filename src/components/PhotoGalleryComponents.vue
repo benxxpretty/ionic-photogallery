1d27698ee8ab417883d21f4f53caa6d58dad8e45
vinafernandez03@gmail.com <vinafernandez03@gmail.com>
2026-09-12 12:56:11 +0800

    Initial Commit

---
+++ src/components/PhotoGalleryComponents.vue
@@ -0,0 +1,59 @@
+<template> 
+  <ion-card> 
+    <ion-card-header> 
+      <ion-card-title>Photo Gallery</ion-card-title> 
+    </ion-card-header> 
+    <ion-card-content> 
+      <div v-if="photos.length === 0" class="empty-gallery"> 
+        <p>No picture yet.</p> 
+        <p>Take a picture using the camera</p> 
+      </div> 
+      <ion-grid v-else> 
+        <ion-row> 
+          <ion-col 
+            v-for="(photo, index) in photos" 
+            :key="index" 
+            size="6"
+            size-md="4" 
+          > 
+            <ion-card> 
+              <ion-img :src="photo" class="gallery-image" /> 
+            </ion-card> 
+          </ion-col> 
+        </ion-row> 
+      </ion-grid> 
+    </ion-card-content> 
+  </ion-card> 
+</template> 
+ 
+<script setup lang="ts"> 
+import {  
+    IonCard, 
+    IonCardContent, 
+    IonCardHeader, 
+    IonCardTitle, 
+    IonCol, 
+    IonGrid, 
+    IonImg, 
+    IonRow 
+} from "@ionic/vue"; 
+defineProps<{ photos: string[] }>(); 
+</script> 
+
+<style scoped> 
+.empty-gallery { 
+    text-align:center; 
+    padding: 30px; 
+    color:bisque;  
+} 
+
+.gallery-image { 
+    width: 100%; 
+    height: auto; 
+} 
+
+.gallery-image::part(image) { 
+    object-fit: cover; 
+} 
+</style>
+
